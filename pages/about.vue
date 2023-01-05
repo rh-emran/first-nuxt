85882daed828def18880ab49e800d5aa21aa890b
raihanulhaq5 <raihanulhaq@gmail.com>
2023-01-05 21:44:07 +0600

    bonus first fatch

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,16 @@
 <template>
   <h2>About</h2>
+  <NuxtLink to="/">Back to home</NuxtLink>
+  <div v-if="pending">Loading ...</div>
+  <div v-else>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "about",
-};
+<script setup>
+const { pending, data: users } = useLazyFetch(
+  "https://jsonplaceholder.typicode.com/users"
+);
 </script>
-
-<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <Button>Click Here</Button>
+    <!-- <Button>Click Here</Button> -->
+    <NuxtLink to="/about">About</NuxtLink>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+
+    <ul>
+      <li v-for="user in users2" :key="user.id">{{ user.name }}</li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const users = await $fetch("https://jsonplaceholder.typicode.com/users");
+const { data: users2 } = await useFetch(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
