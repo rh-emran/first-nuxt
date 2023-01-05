aa454bafef9b56d5257d2841e928c74eb411ad3f
raihanulhaq5 <raihanulhaq@gmail.com>
2023-01-05 20:20:54 +0600

    Initial Commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>About</h2>
+</template>
+
+<script>
+export default {
+  name: "about",
+};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <Button>Click Here</Button>
+  </div>
+</template>
